bdcfb0cb8285f65c3ec971e348f662a8c786e879
Unknown <serpre97@gmail.com>
2021-03-07 13:50:31 +0100

    Main Commit - Stable Build

--- README.md
+++
@@ -1,1 +0,0 @@
-IA - Practica 1
---
+++ src/IA/Red/Main.java
@@ -0,0 +1,11 @@
+package IA.Red;
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+		Test newtest = new Test();
+		newtest.main(null);
+	}
+
+}
